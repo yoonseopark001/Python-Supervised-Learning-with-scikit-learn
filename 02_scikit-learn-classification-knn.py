027d6a6e219124bfe4dd5f212c26f1c10bd3a13c
Yoonseo Park <yoonseopark001@gmail.com>
2019-12-30 22:10:44 +0900

    Update 02_scikit-learn-classification-knn.py

--- 02_scikit-learn-classification-knn.py
+++ 02_scikit-learn-classification-knn.py
@@ -1,4 +1,25 @@
-02
+
+# Import KNeighborsClassifier from sklearn.neighbors
+from sklearn.neighbors import KNeighborsClassifier 
+
+# Create arrays for the features and the response variable
+y = df['party'].values
+X = df.drop('party', axis=1).values
+
+# Create a k-NN classifier with 6 neighbors: knn
+knn = KNeighborsClassifier(n_neighbors = 6)
+
+# Fit the classifier to the data
+knn.fit(X, y)
+
+# Predict the labels for the training data X
+y_pred = knn.predict(X)
+
+# Predict and print the label for the new data point X_new
+new_prediction = knn.predict(X_new)
+print("Prediction: {}".format(new_prediction))
+
+=======================================================
 
 from sklearn.neighbors import KNeighborsClassifier
 knn = KNeighborsClassifier(n_neighbors = 6)
@@ -11,5 +32,4 @@ prediction = knn.predict(X_new)
 X_new.shape
 
 print('Prediction {}'.format(prediction))
-
 =======================================================
