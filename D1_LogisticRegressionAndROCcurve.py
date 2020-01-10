60029a69b0c0b993d380e1495986466fea7f8d8a
Yoonseo Park <yoonseopark001@gmail.com>
2020-01-10 17:32:21 +0900

    Update and rename D1_LogisticRegressionAndROCcurve to D1_LogisticRegressionAndROCcurve.py

--- D1_LogisticRegressionAndROCcurve.py
+++ D1_LogisticRegressionAndROCcurve.py
@@ -6,7 +6,8 @@ from sklearn.linear_model import logisticRegression
 from sklearn.model_selection import train_test_split
 
 logreg = LogisticRegression()
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, 
+                                   test_size=0.4, random_state=42)
 logreg.fit(X_train, y_train)
 y_pred = logreg.predict(X_test)
 
