aea9c77dcedbc59a0fe4c88baadfaac6ffe915a7
Yoonseo Park <yoonseopark001@gmail.com>
2020-01-14 23:40:35 +0900

    Update D1_LogisticRegressionAndROCcurve.py

--- D1_LogisticRegressionAndROCcurve.py
+++ D1_LogisticRegressionAndROCcurve.py
@@ -11,6 +11,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y,
 logreg.fit(X_train, y_train)
 y_pred = logreg.predict(X_test)
 
+================================================================================
+
 # plotting the ROC curve (the receiver operating characteristic curve)
 
 from sklearn.metrics import roc_curve
@@ -27,4 +29,30 @@ plt.show()
 
 # Probability thresholds
 # by default, logistic regression threshold = 0.5
+
+================================================================================
+
+# AUC: Area under the ROC curve
+# The larger area under the ROC curve, the better model
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score
+
+logreg = LogisticRegression()
+X_train, X_test, y_train, y_test = train_test_split(X, y, 
+                                   test_size=0.4, random_state=42)
+logreg.fit(X_train, y_train)
+y_pred_prob = logreg.predict_proba(X_test)[:,1] # pass the true label
+
+roc_auc_score(y_test, y_pred_prob)
+
+================================================================================
+
+# AUC using cross-validation
+
+from sklearn.model_selection import cross_val_score
+cv_scores = cross_val_score(logreg, X, y, cv=5,
+                            scoring='roc_auc')
+print(cv_scores)
 
